62accfd080b0fa1b085f37e713aa053ef240cd0f
mutho-knee <mwangimichelle911@gmail.com>
2023-04-06 09:18:58 +0300

    first commit

---
+++ app/src/main/java/com/muthoknee/morningsimplelists/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.muthoknee.morningsimplelists
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mContacts:ListView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mContacts = findViewById(R.id.mListContact)
+        mContacts.setOnItemClickListener { adapterView, view, i, l ->
+            val name = adapterView.getItemAtPosition(i).toString()
+            Toast.makeText(this,name,Toast.LENGTH_LONG).show()
+        }
+    }
+}
